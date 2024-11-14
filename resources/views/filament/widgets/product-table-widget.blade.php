8873fb2cb2cf44b937d893ce22eb20b8bb67fa49
rizky-gumelar <111202113304@mhs.dinus.ac.id>
2024-11-14 14:45:03 +0700

    fix table darkmode

--- resources/views/filament/widgets/product-table-widget.blade.php
+++ resources/views/filament/widgets/product-table-widget.blade.php
@@ -1,23 +1,30 @@
-<div class="card bg-white shadow-md rounded-lg overflow-hidden w-full">
+<div class="card bg-white dark:bg-gray-900 shadow-md rounded-lg overflow-hidden w-full">
     <div class="card-body p-4">
         <div class="overflow-x-auto">
-            <table class="w-full border-collapse border border-gray-200 min-w-full">
+            <table class="w-full border-collapse border border-gray-200 dark:border-gray-700 min-w-full">
                 <thead>
-                    <tr class="bg-gray-100 border-b border-gray-200">
-                        <th class="px-4 py-2 text-left text-sm font-semibold text-gray-700">Denom / Brand</th>
+                    <tr class="bg-gray-100 dark:bg-gray-800 border-b border-gray-200 dark:border-gray-700">
+                        <th class="px-4 py-2 text-left text-sm font-semibold text-gray-700 dark:text-gray-200">
+                            Denom / Brand
+                        </th>
                         @foreach ($brands as $brand)
-                        <th class="px-4 py-2 text-left text-sm font-semibold text-gray-700">{{ $brand['brand_name'] }}</th>
+                        <th class="px-4 py-2 text-left text-sm font-semibold text-gray-700 dark:text-gray-200">
+                            {{ $brand['brand_name'] }}
+                        </th>
                         @endforeach
                     </tr>
                 </thead>
                 <tbody>
                     @foreach ($denoms as $denom)
-                    <tr class="border-b border-gray-200 hover:bg-gray-50">
-                        <th class="px-4 py-2 text-sm font-semibold text-gray-700 bg-gray-100"> <!-- Jadikan kolom pertama header -->
+                    <tr class="border-b border-gray-200 dark:border-gray-700">
+                        <th class="px-4 py-2 text-sm font-semibold text-gray-700 dark:text-gray-200 bg-gray-100 dark:bg-gray-900">
+                            <!-- Kolom pertama sebagai header untuk setiap row -->
                             {{ $denom->denom }}
                         </th>
                         @foreach ($brands as $brand)
-                        <td class="px-4 py-2 text-sm text-gray-700">{!! $this->tableData[$denom->denom][$brand['brand_name']] !!}</td>
+                        <td class="px-4 py-2 text-sm text-gray-700 dark:text-gray-200">
+                            {!! $this->tableData[$denom->denom][$brand['brand_name']] !!}
+                        </td>
                         @endforeach
                     </tr>
                     @endforeach
